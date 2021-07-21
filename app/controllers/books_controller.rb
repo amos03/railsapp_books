f2d8248b63e4bb36b8e6a748f1c7371646469132
amos03 <amossi@gmail.com>
2021-07-21 16:02:01 -0400

    contact_params and create methods in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,16 @@ class BooksController < ApplicationController
     end
 
     def new
+        @book=Book.new
     end
 
     def create
+        @book = Book.new(contact_params)
+        if @book.save 
+            redirect_to books_url
+        else 
+            render :new
+        end
     end
 
     def update
@@ -22,4 +29,10 @@ class BooksController < ApplicationController
 
     def destroy
     end
+
+    private
+    def contact_params
+        params.require(:book).permit(:author, :title, :isbn, :cover_url) 
+    end
+
 end
